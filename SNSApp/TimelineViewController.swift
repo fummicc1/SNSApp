bfd4a464b2f750f5d952c669996678f5c67fd722
fummicc1 <fumiya.tennis1@gmail.com>
2019-08-29 19:40:18 +0900

    メール認証編

---
+++ SNSApp/TimelineViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+import Firebase
+
+class TimelineViewController: UIViewController {
+
+    var user: User!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
